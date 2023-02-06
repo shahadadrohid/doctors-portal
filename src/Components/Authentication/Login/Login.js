f0b84bacdef48fa8908c7d397bdf12ebd771dbdb
Mohammad Shahadad Hossain <shahadadrohid@gmail.com>
2023-02-06 20:39:42 +0600

    Login form with validation using react hooks form

--- src/Components/Authentication/Login/Login.js
+++ src/Components/Authentication/Login/Login.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useForm } from 'react-hook-form';
 import auth from '../../../firbase.init';
 
 const Login = () => {
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+
+    const { register, formState: { errors }, handleSubmit } = useForm();
 
     if (user) {
         console.log(user)
@@ -11,12 +15,70 @@ const Login = () => {
     if (error) {
         console.log(error)
     }
+    const onSubmit = data => {
+        console.log(data)
+    }
     return (
         <div className="flex h-screen justify-center items-center">
             <div className="card w-96 bg-base-100 shadow-xl">
                 <div className="card-body">
                     <h2 className="text-4xl font-bold text-primary text-center">Login</h2>
-                    <button className="btn btn-accent text-white">LOGIN</button>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+
+                        {/* Input email field with validation */}
+                        <div class="form-control w-full max-w-xs">
+                            <label class="label">
+                                <span class="label-text">Email</span>
+                            </label>
+
+                            <input
+                                type="email" placeholder="Email Address" class="input input-bordered w-full max-w-xs"
+                                {...register("email", {
+                                    required: {
+                                        value: true,
+                                        message: "Email Address is required"
+                                    },
+                                    pattern: {
+                                        value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                                        message: 'Provide a valid email'
+                                    }
+                                })}
+                            />
+                            <label class="label">
+                                {errors.email?.type === 'required' && <span class="label-text-alt text-red-500">{errors.email.message}</span>}
+                                {errors.email?.type === 'pattern' && <span class="label-text-alt text-red-500">{errors.email.message}</span>}
+                            </label>
+                        </div>
+
+                        {/* Input password field with validation */}
+                        <div class="form-control w-full max-w-xs">
+                            <label class="label">
+                                <span class="label-text">Password</span>
+                            </label>
+
+                            <input
+                                type="password" placeholder="Password" class="input input-bordered w-full max-w-xs"
+                                {...register("password", {
+                                    required: {
+                                        value: true,
+                                        message: "Password is required"
+                                    },
+                                    midLength: {
+                                        value: 6,
+                                        message: 'Must be 6 characters or longer'
+                                    }
+                                })}
+                            />
+                            <label class="label">
+                                {errors.password?.type === 'required' && <span class="label-text-alt text-red-500">{errors.password.message}</span>}
+                                {errors.password?.type === 'midLength' && <span class="label-text-alt text-red-500">{errors.password.message}</span>}
+                            </label>
+                        </div>
+
+                        <input
+                            className="btn btn-accent w-full text-white" type="submit" value="login" />
+
+                    </form>
                     <div className="divider">OR</div>
 
                     <button
